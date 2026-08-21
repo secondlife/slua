71af4f55e1fb48848732cc61ba6c16ef89c668ba
Harold Cindy <120691094+HaroldCindy@users.noreply.github.com>
2026-08-21 15:13:55 -0700

    Use existing vformat implementation from StringUtils

--- Executor/src/Logging.cpp
+++ Executor/src/Logging.cpp
@@ -1,30 +1,14 @@
 #include "Luau/Executor.h"
 
+#include "Luau/StringUtils.h"
+
 #include <cstdarg>
-#include <cstdio>
 #include <string>
 
 namespace Luau
 {
 namespace Executor
 {
-
-static std::string vformat(const char* fmt, va_list args)
-{
-    va_list measure;
-    va_copy(measure, args);
-    int needed = vsnprintf(nullptr, 0, fmt, measure);
-    va_end(measure);
-
-    if (needed <= 0)
-        return std::string();
-
-    std::string out;
-    out.resize((size_t)needed + 1);
-    vsnprintf(&out[0], out.size(), fmt, args);
-    out.pop_back();
-    return out;
-}
 
 static void vlog(LogLevel level, const char* source, const char* fmt, va_list args)
 {
